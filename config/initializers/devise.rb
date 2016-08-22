5833aefa326b1b832a49fe5b6e0243b133833073
Prelang Builder <builder@prelang.com>
2016-08-22 09:33:16 +0000

    Configured the Devise initializer with Facebook app keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,4 +271,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, "551447711686266", ENV['FACEBOOK_SECRET_ID']
 end
